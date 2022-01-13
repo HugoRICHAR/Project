376ad047e666fc692492cdee939eee0fb6cad1b7
Hugo RICHARD <hugz@mbp-de-hugo.home>
2022-01-13 18:36:49 +0100

    100 premières pages

---
+++ test_ML.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from pandas.plotting import scatter_matrix
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OrdinalEncoder
+
+
+housing=pd.read_csv("housing.csv")
+housing.hist(bins=50,figsize=(20,15))
+
+def split_train_test(data, test_ratio):
+    shuffled_indices=np.random.permutation(len(data))
+    test_set_size=int(len(data)*test_ratio)
+    test_indices=shuffled_indices[:test_set_size]
+    train_indices=shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+train_set, test_set=train_test_split(housing,test_size=0.2,random_state=42)
+
+housing.plot(kind="scatter",x="longitude",y="latitude",alpha=0.4,s=housing["population"]/100, label="population",figsize=(10,7),c="median_house_value",cmap=plt.get_cmap("jet"),colorbar=True)
+plt.show()
+
+corr_matrix =housing.corr()
+
+attributes=["median_house_value","median_income","total_rooms","housing_median_age"]
+scatter_matrix(housing[attributes],figsize=(12,8))
+
+imputer=SimpleImputer(strategy="median")
+housing_num=housing.drop("ocean_proximity",axis=1)
+imputer.fit(housing_num)
+imputer.statistics_
+X=imputer.transform(housing_num)
+housing_tr=pd.DataFrame(X,columns=housing_num.columns,index=housing_num.index)
+
+housing_cat=housing[["ocean_proximity"]]
+housing_cat.head(10)
+
+ordinal_encoder=OrdinalEncoder()
+housing_cat_encoded=ordinal_encoder.fit_transform(housing_cat)
+housing_cat_encoded[:10]
+ordinal_encoder.categories_
+
+from sklearn.preprocessing import OneHotEncoder
+
+cat_encoder=OneHotEncoder()
+housing_cat_1hot=cat_encoder.fit_transform(housing_cat)
+housing_cat_1hot.toarray()
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+rooms_ix, bedrooms_ix, population_ix, households_ix= 3,4,5,6
+
+class CombinedAttributesAdder(BaseEstimator,TransformerMixin):
+    def __init__(self, add_bedrooms_per_room=True):
+        self.add_bedrooms_per_room=add_bedrooms_per_room
+    def fit(self,X,y=None):
+        return self
+    def transform(self,X,y=None):
+        rooms_per_household=X[:,rooms_ix]/X[:,households_ix]
+        population_per_household= X[:,population_ix] /X[:,households_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room=X[:,bedrooms_ix]/X[:,rooms_ix]
+            return np.c_[X,rooms_per_household,population_per_household,bedrooms_per_room]
+        else:
+            return np.c_[X,rooms_per_household,population_per_household]
+
+attr_adder=CombinedAttributesAdder(add_bedrooms_per_room=False)
+housing_extra_attribs=attr_adder.transform(housing.values)
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+num_pipeline=Pipeline([('imputer',SimpleImputer(strategy="median")),('attribs_adder',CombinedAttributesAdder()),('std_scaler',StandardScaler())])
+housing_num_tr=num_pipeline.fit_transform(housing_num)
+
+from sklearn.compose import ColumnTransformer
+
+
